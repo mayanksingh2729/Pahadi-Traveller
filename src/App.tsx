99da048a0b53f47b48d1c4184040fac9e7e5a206
mayanksingh2729 <mayanksingh2729@gmail.com>
2025-04-18 09:49:44 +0530

    Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
+import { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Index from "./pages/Index";
 import Destinations from "./pages/Destinations";
 import DestinationDetail from "./pages/DestinationDetail";
@@ -12,6 +12,18 @@ import BlogPost from "./pages/BlogPost";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-DKLSMPD7PM"); // Replace with your ID
+ReactGA.send("pageview"); // Sends initial pageview
+function usePageTracking() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+}
+
 
 const queryClient = new QueryClient();
 
